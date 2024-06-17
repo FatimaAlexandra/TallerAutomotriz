f9687da1dbdc2769093499048aa9e02ad9432e4e
FatimaAlexandra <falexandra@outlook.es>
2024-06-16 23:25:06 -0600

    no sirve agregar servicios realizados

--- Controllers/ServicioRealizadoController.cs
+++ Controllers/ServicioRealizadoController.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Linq;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using amazon.Models;
-using Microsoft.EntityFrameworkCore;
-
+using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace amazon.Controllers
 {
@@ -22,145 +20,26 @@ namespace amazon.Controllers
         public IActionResult Index()
         {
             var serviciosRealizados = _context.ServicioRealizado
-                .Include(s => s.Servicio)
-                .Include(s => s.Usuario)
+                .Include(sr => sr.Servicio)
+                .Include(sr => sr.Usuario)
                 .ToList();
+
             return View(serviciosRealizados);
-        }
-
-        // GET: ServicioRealizado/Details/5
-        public IActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var servicioRealizado = _context.ServicioRealizado
-                .Include(s => s.Servicio)
-                .Include(s => s.Usuario)
-                .FirstOrDefault(m => m.Id == id);
-
-            if (servicioRealizado == null)
-            {
-                return NotFound();
-            }
-
-            return View(servicioRealizado);
         }
 
         // GET: ServicioRealizado/Create
         public IActionResult Create()
         {
             ViewBag.ServicioId = new SelectList(_context.Servicios, "Id", "Nombre");
-            ViewBag.UsuarioId = new SelectList(_context.Usuarios, "Id", "Nombre");
             return View();
         }
 
-        // POST: ServicioRealizado/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Create([Bind("Id,ServicioId,UsuarioId,Precio,Fecha")] ServicioRealizado servicioRealizado)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(servicioRealizado);
-                _context.SaveChanges();
-                return RedirectToAction(nameof(Index));
-            }
-            ViewBag.ServicioId = new SelectList(_context.Servicios, "Id", "Nombre", servicioRealizado.ServicioId);
-            ViewBag.UsuarioId = new SelectList(_context.Usuarios, "Id", "Nombre", servicioRealizado.UsuarioId);
-            return View(servicioRealizado);
-        }
+        
 
-        // GET: ServicioRealizado/Edit/5
-        public IActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var servicioRealizado = _context.ServicioRealizado.Find(id);
-            if (servicioRealizado == null)
-            {
-                return NotFound();
-            }
-            ViewBag.ServicioId = new SelectList(_context.Servicios, "Id", "Nombre", servicioRealizado.ServicioId);
-            ViewBag.UsuarioId = new SelectList(_context.Usuarios, "Id", "Nombre", servicioRealizado.UsuarioId);
-            return View(servicioRealizado);
-        }
-
-        // POST: ServicioRealizado/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Edit(int id, [Bind("Id,ServicioId,UsuarioId,Precio,Fecha")] ServicioRealizado servicioRealizado)
-        {
-            if (id != servicioRealizado.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(servicioRealizado);
-                    _context.SaveChanges();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!ServicioRealizadoExists(servicioRealizado.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            ViewBag.ServicioId = new SelectList(_context.Servicios, "Id", "Nombre", servicioRealizado.ServicioId);
-            ViewBag.UsuarioId = new SelectList(_context.Usuarios, "Id", "Nombre", servicioRealizado.UsuarioId);
-            return View(servicioRealizado);
-        }
-
-        // GET: ServicioRealizado/Delete/5
-        public IActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var servicioRealizado = _context.ServicioRealizado
-                .Include(s => s.Servicio)
-                .Include(s => s.Usuario)
-                .FirstOrDefault(m => m.Id == id);
-
-            if (servicioRealizado == null)
-            {
-                return NotFound();
-            }
-
-            return View(servicioRealizado);
-        }
-
-        // POST: ServicioRealizado/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public IActionResult DeleteConfirmed(int id)
-        {
-            var servicioRealizado = _context.ServicioRealizado.Find(id);
-            _context.ServicioRealizado.Remove(servicioRealizado);
-            _context.SaveChanges();
-            return RedirectToAction(nameof(Index));
-        }
 
         private bool ServicioRealizadoExists(int id)
         {
-            return _context.ServicioRealizado.Any(e => e.Id == id);
+            return _context.ServicioRealizado.Any(sr => sr.Id == id);
         }
     }
 }
--- Models/ServicioRealizado.cs
+++ Models/ServicioRealizado.cs
@@ -5,10 +5,11 @@ namespace amazon.Models
     public class ServicioRealizado
     {
         public int Id { get; set; }
-        public int ServicioId { get; set; } // Cambiado de IdServicio a ServicioId
+        public int ServicioId { get; set; } 
         public int UsuarioId { get; set; }
         public decimal Precio { get; set; }
-        public DateTime Fecha { get; set; }
+        public String Fecha { get; set; }
+        public int Estado { get; set; } = 1; //Valor por defecto
 
         // Propiedades de navegación
         public Servicio Servicio { get; set; }
