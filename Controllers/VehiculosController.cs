6278fa9a513c8884b01cc8d4a6ac823ab124758c
Fatima Alexandra Castillo Martinez <cm17102@ti.ues.edu.sv>
2024-09-14 10:28:37 -0600

    Ver vehiculos

---
+++ Controllers/VehiculosController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using amazon.Models;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace amazon.Controllers
+{
+    public class VehiculosController : Controller
+    {
+        private readonly DbamazonContext _context;
+
+        public VehiculosController(DbamazonContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Vehiculos
+        public async Task<IActionResult> Index()
+        {
+            var vehiculos = await _context.Vehiculos
+                .Include(v => v.Usuario)
+                .ToListAsync();
+            return View("Index", vehiculos); // Asegúrate de que el nombre de la vista sea correcto
+        }
+    }
+}
---
+++ Models/Vehiculo.cs
@@ -0,0 +1,17 @@
+﻿namespace amazon.Models
+{
+    public class Vehiculo
+    {
+        public int Id { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public int Año { get; set; }
+        public string Placa { get; set; }
+        public string Descripcion { get; set; } // Descripción opcional
+        public int UsuarioId { get; set; }
+
+        // Relación con otras tablas
+        public Usuario Usuario { get; set; }
+
+    }
+}
