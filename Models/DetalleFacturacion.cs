5e2db7cd1118c36db087c811f9b14f2006ba5231
Fatima Alexandra Castillo Martinez <cm17102@ti.ues.edu.sv>
2024-09-30 16:20:18 -0600

    Vista Factura

---
+++ Models/DetalleFacturacion.cs
@@ -0,0 +1,22 @@
+﻿using amazon.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace TuNamespace.Models
+{
+    public class DetalleFacturacion
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int FacturacionId { get; set; }
+
+        [Required]
+        public int ServicioRealizadoId { get; set; }
+
+        [Required]
+        public decimal Subtotal { get; set; }
+
+        public virtual Facturacion Facturacion { get; set; }  // Relación con la tabla de Facturación
+        public virtual ServicioRealizado ServicioRealizado { get; set; }  // Relación con el Servicio Realizado
+    }
+}
---
+++ Models/Facturacion.cs
@@ -0,0 +1,31 @@
+﻿using amazon.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace TuNamespace.Models
+{
+    public class Facturacion
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int UsuarioId { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime FechaFacturacion { get; set; }
+
+        [Required]
+        [DataType(DataType.Currency)]
+        public decimal MontoTotal { get; set; }
+
+        [Required]
+        public string MetodoPago { get; set; }
+
+        public string EstadoFactura { get; set; } = "Pendiente";
+
+        public virtual Usuario Usuario { get; set; }  // Relación con la tabla de Usuarios
+        public virtual ICollection<DetalleFacturacion> DetalleFacturacion { get; set; }  // Detalles de la factura
+    }
+}
